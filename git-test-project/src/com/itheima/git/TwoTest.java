91fcfd2682e2e0747ed45705824fe01f16235901
黄剑飞 <mr.flyfly@qq.com>
2019-03-15 21:55:12 +0800

    把本地工程添加到git

---
+++ git-test-project/src/com/itheima/git/TwoTest.java
@@ -0,0 +1,12 @@
+package com.itheima.git;
+
+public class TwoTest {
+    public static void main(String[] args) {
+        System.out.println("哈哈");
+        System.out.println("哈哈");
+        System.out.println("哈哈");
+        System.out.println("哈哈");
+        System.out.println("哈哈");
+        System.out.println("哈哈");
+    }
+}
